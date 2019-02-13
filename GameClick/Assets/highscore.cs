8a70cd187892a75bd72b4096b8aa017ff78e374e
Sabri Sangjaya <31299056+sabrisangjaya@users.noreply.github.com>
2019-02-13 14:18:50 +0700

    first commit
    first Commit

---
+++ GameClick/Assets/highscore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class highscore : MonoBehaviour {
+	public Text txtHighscore;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txtHighscore.text="Highscore : "+PlayerPrefs.GetInt("highscore");
+	}
+
+	public void playGame(){
+		SceneManager.LoadScene ("gameplay");
+	}
+
+	public void resetHigh(){
+		PlayerPrefs.SetInt("highscore",0);
+	}
+		
+}
